2e7e74dd22efc47f46cf42fc40aa2d86ba228464
madelinehebert <160977018+madelinehebert@users.noreply.github.com>
2025-07-20 12:12:18 -0400

    Update build.sh
    added multijob support

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ python3 -m venv "$PyQt6-Tribblix-venv"
 source "$VENV_NAME/bin/activate"
 
 # Compile from source - from https://www.riverbankcomputing.com/static/Docs/PyQt6/installation.html
-pip -v install --config-settings --confirm-license= --config-settings --qmake=/usr/versions/Qt-6/bin/qmake PyQt6
+pip -v install --config-settings --confirm-license= --config-settings --qmake=/usr/versions/Qt-6/bin/qmake PyQt6 --config-settings --jobs $(nrpoc)
 
 # Build a wheel of PyQt6 and PyQt6.sip
 python3 -m pip wheel PyQt6
